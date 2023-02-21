1df9b6b4f21b4c3e5aa6a0d43e6a0364ee65269c
Shubham Kaushik <skaushik274@gmail.com>
2023-02-21 20:40:19 +0530

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -117,12 +117,14 @@ class Question(models.Model):
     course = models.ManyToManyField(Course)
     # <HINT> A sample model method to calculate if learner get the score of the question
     def is_get_score(self, selected_ids):
-       all_answers = self.choice_set.filter(is_correct=True).count()
-       selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-       if all_answers == selected_correct:
-           return True
-       else:
-           return False
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+    def __str__(self):
+        return self.question_text 
 
 
 #  <HINT> Create a Choice Model with:
@@ -139,7 +141,9 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(null=False, max_length=200)
     course = models.ManyToManyField(Course)
-    
+    is_correct = models.BooleanField(default=False)
+    def __str__(self):
+        return self.choice_text
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
